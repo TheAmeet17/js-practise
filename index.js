f7b4ee1126d6a8d76785362bb9ef78d5646359bb
ameet <ameet@gmail.com>
2024-03-23 12:11:03 +0545

    object and function

---
+++ index.js
@@ -0,0 +1,89 @@
+// function sum(a,b)
+// {
+//     return a+b;
+// }
+// console.log(sum(5,7));
+
+
+// function average(b,c)
+// {
+//     const a=(b+c)/2;
+//     return a;
+// }
+// let result=average (5,4);
+// console.log(result);
+
+// function amit(a,b)
+// {
+//     const rest=a*b;
+//     return rest;
+    
+// }
+// let result=amit(5,4);
+//     console.log(result);
+
+// function ameet(b,c,e,t)
+// {
+//     let a=b*c/e+t;
+//     return a;
+// }
+// let result=ameet(4,5,2,3)
+// console.log(result);
+
+
+// function sum(a,b){
+//     let s=a+b;
+//     return s;
+// }
+// let result=sum(5,6);
+// console.log(result);
+
+
+// console.log(sum(8,9));
+
+// console.log("Ameet")
+// let city={
+//     Name:"new york",
+//     famous:"california",
+//     friends:"nissan",
+//     "mobile no":9844256261,
+//     fruits:{
+//         fav:"mango",
+//         "not fav":"banana",
+//     },
+// };
+// console.log(city.Name)
+// console.log(city.friends);
+// console.log(city.fruits["not fav"])
+
+
+// let a;
+// console.log(typeof a)
+
+let student={
+    Name:"ameet",
+    class:10,
+    "mobile no":9873646n,
+    isvalidforvote:true,
+    
+};
+student.marks=50;
+console.log(student.marks);
+delete student.class;
+
+
+let i=7
+;
+while(i>5){
+    console.log("cat");
+    i--
+}
+
+function add(a,b,c,d)
+{
+    
+    console.log(a+b+c+d);
+  //  return z;
+}
+add(7,8,9,8)
+//console.log(add);
